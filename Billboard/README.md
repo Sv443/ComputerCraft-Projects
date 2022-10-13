0a60ce6ce101ba2dc29d2ff2a89ae35a24a48c8c
Sv443 <contact@sv443.net>
2022-10-13 16:53:44 +0200

    feat: billboard code

--- Billboard/README.md
+++ Billboard/README.md
@@ -1,7 +1,10 @@
 # Billboard
 Displays some text and cycling colors on a monitor  
 
+<!--
 <br>
+
+TODO:
 
 ![screenshot](./screenshot.png)
 
@@ -9,17 +12,19 @@ Displays some text and cycling colors on a monitor
 
 ---
 
+-->
+
 <br>
 
 ### Install
-1. Craft a computer and six monitors in-game
+1. Craft a computer and eight monitors in-game
 2. Place the monitors in a 4x2 (w x h) and make sure the computer touches one of the monitors
-3. Open the computer and enter these commands:
+3. Open the computer and run these commands:
 ```
 label set billboard
-pastebin get TODO startup
+pastebin get UGxeuMGi startup
 edit startup.lua
 ```
 4. Edit settings found at the top to your needs
-5. Press <kbd>Ctrl</kbd>, select [Save], <kbd>Return</kbd>, <kbd>Ctrl</kbd> again, select [Exit], then <kbd>Return</kbd>
+5. Press <kbd>Ctrl</kbd>, select `[Save]`, then <kbd>Return</kbd>, then press <kbd>Ctrl</kbd> again, select `[Exit]`, then <kbd>Return</kbd>
 6. Run `reboot` to start the program
---
+++ Billboard/billboard.lua
@@ -0,0 +1,87 @@
+-- in seconds - how often to update display & switch colors
+UPDATE_UPDATE_INTERVAL = 2
+-- side of the PC that touches the monitor
+MONITOR_SIDE = "right"
+
+-- TODO: make this a table - nil = empty line & transparent bg
+-- LINES = { nil, "line1", "line2", nil }
+
+-- 14 character limit per line with TEXT_SCALE = 3 and monitor width = 4 blocks
+LINE_1 = "Hello, I'm un-"
+LINE_2 = "der the water "
+
+-- text size, has to be a multiple of 0.5
+TEXT_SCALE = 3
+-- horizontal starting position of text
+TEXT_X_POS = 1
+-- text color
+TEXT_COLOR = colors.gray
+-- color of background of text lines, set to nil to also cycle this color
+TEXT_BG_COLOR = colors.white
+
+-- background colors to cycle through
+BG_COLORS = {
+    colors.orange,
+    colors.red,
+    colors.purple,
+    colors.blue,
+    colors.cyan,
+    colors.green,
+    colors.lime,
+    colors.yellow
+}
+
+bgcIdx = 1
+
+-- updates the monitor
+function update(monitor)
+    local bgCol = BG_COLORS[bgcIdx]
+    bgcIdx = bgcIdx + 1
+
+    if bgcIdx > tableLength(BG_COLORS) then
+        bgcIdx = 1
+    end
+
+    monitor.clear()
+    monitor.setTextScale(TEXT_SCALE)
+    monitor.setCursorPos(TEXT_X_POS, 2)
+    monitor.setBackgroundColor(bgCol)
+
+    monitor.setTextColor(TEXT_COLOR)
+    if TEXT_BG_COLOR ~= nil then
+        monitor.setBackgroundColor(TEXT_BG_COLOR)
+    end
+
+    monitor.write(LINE_1)
+    monitor.setBackgroundColor(bgCol)
+
+    monitor.setTextColor(TEXT_COLOR)
+    if TEXT_BG_COLOR ~= nil then
+        monitor.setBackgroundColor(TEXT_BG_COLOR)
+    end
+
+    monitor.setCursorPos(TEXT_X_POS, 3)
+    monitor.write(LINE_2)
+    monitor.setBackgroundColor(bgCol)
+end
+
+-- returns the length of a table
+function tableLength(table)
+    local count = 0
+    for _ in pairs(table) do
+        count = count + 1
+    end
+    return count
+end
+
+-- main program loop
+function run()
+    local monitor = peripheral.wrap(MONITOR_SIDE)
+
+    while true do
+        update(monitor)
+        os.sleep(UPDATE_INTERVAL)
+    end
+end
+
+run()
