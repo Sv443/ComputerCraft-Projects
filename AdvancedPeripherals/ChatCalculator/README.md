57efb913fa1665641c759857f5fc1f04bc9c8041
Sv443 <contact@sv443.net>
2025-05-27 01:24:17 +0200

    fix: better chatcalc

--- AdvancedPeripherals/ChatCalculator/README.md
+++ AdvancedPeripherals/ChatCalculator/README.md
@@ -1,12 +1,21 @@
 ## ChatCalculator
 This program listens to chat messages with a certain prefix, evaluates the mathematical expression, and replies with the result.  
 It can be used to quickly calculate simple math problems without having to open a calculator or use a website.  
-Parentheses and order of operations are not supported. All operations are done left to right.
+Parentheses and order of operations are not supported. All operations are strictly done left to right.
+
+<br>
+
+### Usage:
+Send a chat message with the prefix `!calc`, followed by a mathematical expression, e.g. `!calc 2 + 2 * 2`  
+The supported operators are: `+`, `-`, `*`, `/`, `%`, `^` or `**`, `root` or `r`  
+  
+- Specify the number of the root after the operator, e.g. `!calc 1 + 8 root 2` for the square root of 9, which is 3
+- The expression is always evaluated left to right, so `!calc 2 + 2 * 2` will yield `8`, not `6`
 
 <br>
 
 ### Installation:
-1. Craft a computer and chat box peripheral and place them next to each other
+1. Craft a computer and chat box and place them next to each other
 2. Run these commands:
 ```
 label set ChatCalculator
--- AdvancedPeripherals/ChatCalculator/startup.lua
+++ AdvancedPeripherals/ChatCalculator/startup.lua
@@ -44,12 +44,16 @@ RUN_CMDS = {
                 value = value % num
             elseif operator == "^" or operator == "**" then
                 value = value ^ num
-            elseif operator == "sqrt" then
-                if num < 0 then
+            elseif operator == "root" or operator == "r" then
+                if value < 0 then
                     chat.sendMessageToPlayer("Cannot take square root of a negative number.", ctx.username, "&cCALC", nil, "&4")
                     return
                 end
-                value = math.sqrt(num)
+                if num <= 0 then
+                    chat.sendMessageToPlayer("Cannot take " .. num .. "-th root.", ctx.username, "&cCALC", nil, "&4")
+                    return
+                end
+                value = value ^ (1 / num)
             else
                 chat.sendMessageToPlayer("Unknown operator: " .. tostring(operator), ctx.username, "&cCALC", nil, "&4")
                 return
@@ -60,6 +64,27 @@ RUN_CMDS = {
         chat.sendMessageToPlayer("Result: " .. tostring(value), ctx.username, "&eCALC", nil, "&6")
     end
 }
+
+function b64dec(data)
+    local b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+    data = string.gsub(data, "[^"..b.."=]", "")
+ 
+    return (data:gsub(".", function(x)
+        if x == "=" then return "" end
+        local r, f = "", (b:find(x) - 1)
+        for i = 6, 1, -1 do
+            r = r .. (f % 2 ^ i - f % 2 ^ (i - 1) > 0 and "1" or "0")
+        end
+        return r
+    end):gsub("%d%d%d?%d?%d?%d?%d?%d?", function(x)
+        if #x ~= 8 then return "" end
+        local c = 0
+        for i = 1, 8 do
+            c = c + (x:sub(i, i) == "1" and 2 ^ (8 - i) or 0)
+        end
+        return string.char(c)
+    end))
+end
 
 function run()
     local event, username, message, uuid, isHidden = os.pullEvent("chat")
@@ -94,6 +119,11 @@ function run()
         end
     end
 end
+
+-- cause pastebin hates links
+print(b64dec("CnwgQ2hhdENhbGN1bGF0b3IgYnkgU3Y0NDMKfCBodHRwczovL3JlLnN2NDQzLm5ldC9jYy1wcm9qZWN0cwo="))
+print("Type !calc <num1> <operator> <num2> [<operator> <numN> ...] in chat to calculate.\n")
+print("Operators: +, -, *, /, %, ^ or **, root or r\n")
 
 while true do
     run()
